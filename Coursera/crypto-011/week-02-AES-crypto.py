4501cf474aec90c2050c5bae67a758c7816fdfc2
Sébastien Barbieri <seba@rtbf.be>
2014-07-29 23:36:49 +0200

    added encryption (AES)

--- Coursera/crypto-011/week-02-AES-crypto.py
+++ Coursera/crypto-011/week-02-AES-crypto.py
@@ -2,6 +2,9 @@
 
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
+from Crypto.Hash import SHA
+from Crypto import Random
+import sys
 
 Questions = [
     {'key':'140b41b22a29beb4061bda66b6747e14','ct':'4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81'}
@@ -26,7 +29,6 @@ def ctrdec(question):
     ct = question['ct'][32:]
 
     byteKey = question['key'].decode('hex')
-    byteIv = iv.decode('hex')
     byteCt = ct.decode('hex')
 
     ctr = Counter.new(128,initial_value=long(iv, 16))
@@ -34,20 +36,51 @@ def ctrdec(question):
     msg2 = cipher2.decrypt(byteCt)
     return msg2
 
-for i in range(0,len(Questions)):
-    question = Questions[i]
-    print "Question #:",i+1
-    try:
-        print "CBC dec",cbcdec(question)
-    except Exception, e:
-        print(e)
-    try:
-        print "CTR dec",ctrdec(question)
-    except Exception, e:
-        print(e)
-    
+def cbcenc(text):
+    iv = Random.new().read(AES.block_size)
+    key = Random.new().read(16)
+    print key.encode('hex')
 
-#key = b'Sixteen byte key'
-#iv = Random.new().read(AES.block_size)
-#cipher = AES.new(key, AES.MODE_CFB, iv)
-#msg = iv + cipher.encrypt(b'Attack at dawn')
+    cipher = AES.new(key,AES.MODE_CBC,iv)
+    # pad text
+    topad = 16 - len(text)%16
+    for i in range(0,topad):
+        text += chr(topad)
+
+    message = iv + cipher.encrypt(text)
+    return {'key':key.encode('hex'),'ct':message.encode('hex')}
+
+def ctrenc(text):
+    iv = Random.new().read(AES.block_size)
+    key = Random.new().read(16)
+    print key.encode('hex')
+
+    ctr = Counter.new(128,initial_value=long(iv.encode('hex'), 16))
+    cipher = AES.new(key,AES.MODE_CTR,counter=ctr)
+    message = iv + cipher.encrypt(text)
+    return {'key':key.encode('hex'),'ct':message.encode('hex')}
+
+def decryptMessages(Questions):
+    for i in range(0,len(Questions)):
+        question = Questions[i]
+        print "Question #:",i+1
+        try:
+            print "CBC dec",cbcdec(question)
+        except Exception, e:
+            print(e)
+        try:
+            print "CTR dec",ctrdec(question)
+        except Exception, e:
+            print(e)
+    
+def encryptText(text):
+    print "Text: ",text
+    cbcEncrypted = cbcenc(text)
+    ctrEncrypted = ctrenc(text)
+    print "cbcEncrypted:",cbcEncrypted
+    print "ctrEncrypted:",ctrEncrypted
+    print "decrypted:",cbcdec(cbcEncrypted)
+    print "decrypted:",ctrdec(ctrEncrypted)
+
+
+encryptText(sys.argv[1])
